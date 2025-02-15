08e640a5ff2f726eaea544f0a7761aef37f7d8c3
maeta187 <nexus18789@gmail.com>
2025-02-15 13:06:10 +0900

    🎉 : first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_introduction_to_practice_hands_on_part1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_introduction_to_practice_hands_on_part1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_introduction_to_practice_hands_on_part1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
