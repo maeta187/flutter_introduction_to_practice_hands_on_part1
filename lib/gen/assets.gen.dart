16f9a19c8d620f54eb5a02c77f1555bd758d3e78
maeta187 <nexus18789@gmail.com>
2025-02-15 16:12:47 +0900

    🍱  : assets.genファイルの作成

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,18 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class Assets {
+  const Assets._();
+
+  static const String flipIcon = 'assets/flip_icon.svg';
+  static const String rotateIcon = 'assets/rotate_icon.svg';
+
+  /// List of all assets
+  static List<String> get values => [flipIcon, rotateIcon];
+}
